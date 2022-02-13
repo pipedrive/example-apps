01c6f1fc32a7f4aeb5e529318b60ab1cad7b7da1
Deniss Ivannikov <deniss.ivannikov@pipedrive.com>
2022-02-13 16:20:46 +0200

    DPM-20: Uncomment Oauth vars

--- apps/001-callback-url-oauth/callback.js
+++ apps/001-callback-url-oauth/callback.js
@@ -4,9 +4,9 @@ async function handler(req, res) {
     const apiClient = pipedriveSdk.ApiClient.instance;
 
     let oauth2 = apiClient.authentications.oauth2;
-    // oauth2.clientId = 'clientId'; // Your Pipedrive App OAuth 2 Client ID
-    // oauth2.clientSecret = 'clientSecret'; // Your Pipedrive App OAuth 2 Client Secret
-    // oauth2.redirectUri = 'http://localhost:1800/callback'; // Your Pipedrive App OAuth 2 Redirection endpoint or Callback Uri
+    oauth2.clientId = 'clientId'; // Your Pipedrive App OAuth 2 Client ID
+    oauth2.clientSecret = 'clientSecret'; // Your Pipedrive App OAuth 2 Client Secret
+    oauth2.redirectUri = 'http://localhost:1800/callback'; // Your Pipedrive App OAuth 2 Redirection endpoint or Callback Uri
 
     // Getting authorization code from Pipedrive
     const authCode = req.query.code;
