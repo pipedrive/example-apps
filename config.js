34a3a9aeb707e9178dc6e5766cc9bff174121d2a
Deniss Ivannikov <deniss.ivannikov@pipedrive.com>
2022-03-01 19:25:54 +0200

    DPM-20: Oauth2 config to default values

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 module.exports = {
-    // clientId: 'clientId', // Your Pipedrive App OAuth 2 Client ID
-    clientId: '279541c2bd2a88ec', // Your Pipedrive App OAuth 2 Client ID
-    // clientSecret: 'clientSecret', // Your Pipedrive App OAuth 2 Client Secret
-    clientSecret: 'c4363930a45c57b43623aac3e6a3408c3f8e2bcd', // Your Pipedrive App OAuth 2 Client Secret
-    // redirectUri: 'http://localhost:1800/callback', // Your Pipedrive App OAuth 2 Redirection endpoint or Callback Uri
-    redirectUri: 'https://a545-46-131-61-243.ngrok.io/callback', // Your Pipedrive App OAuth 2 Redirection endpoint or Callback Uri
+    clientId: 'clientId', // Your Pipedrive App OAuth 2 Client ID
+    clientSecret: 'clientSecret', // Your Pipedrive App OAuth 2 Client Secret
+    redirectUri: 'http://localhost:1800/callback', // Your Pipedrive App OAuth 2 Redirection endpoint or Callback Uri
 }
