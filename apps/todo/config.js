ae665ee3163734bb77b493c56bed9db30733a379
Andreas Saltsberg <andreas.saltsberg@pipedrive.com>
2022-03-20 21:54:59 +0200

    DPM-21 replace config.js default

--- apps/todo/config.js
+++ apps/todo/config.js
@@ -2,6 +2,6 @@ module.exports = {
     clientId: 'clientId', // Your Pipedrive App OAuth 2 Client ID
     clientSecret: 'clientSecret', // Your Pipedrive App OAuth 2 Client Secret
     redirectUri: 'https://localhost:3000/callback', // Your Pipedrive App OAuth 2 Redirection endpoint or Callback Uri
-    surfaceJwt: 'helloworld', // The JWT you have to set in Pipedrive Marketplace Manager to check security of Surface request
-    embeddedActionJwt: 'helloworld2', // The JWT you have to set in Pipedrive Marketplace Manager to check security of Embedded action request
+    surfaceJwt: 'surfaceJWTSecret', // The JWT you have to set in Pipedrive Marketplace Manager to check security of Surface request
+    embeddedActionJwt: 'embeddedActionJWTSecret', // The JWT you have to set in Pipedrive Marketplace Manager to check security of Embedded action request
 }
