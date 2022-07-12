cbdbd0d23a1e918e96d3e993a3d23403becee26c
Hem <hemchander.23@gmail.com>
2022-07-12 11:05:31 +0200

    Casing Fix, declaration changes

--- apps/messaging-app-extn-playground/api/core.js
+++ apps/messaging-app-extn-playground/api/core.js
@@ -14,7 +14,7 @@ const router = new Router();
 router.get("/", async (req, res) => {
     let authStatus = false,
         tokenExpiresIn = { expired: true };
-    let domain = await util.getAppDomain();
+    const domain = await util.getAppDomain();
     if (req.user) {
         // We are checking for two things: 1. If user has authorized 2. If authorized token is valid
         tokenExpiresIn = util.getAccessTokenExpiry(req.user.expiry);
@@ -24,48 +24,48 @@ router.get("/", async (req, res) => {
     }
 
     try {
-        let page_data = {
-            authStatus,
+        let pageData = {
+            auth_status,
             domain,
             manifest_data: JSON.stringify(util.generateManifest(domain), null, 4),
             messaging_channel_id_default: process.env.CHANNEL_ID,
             channel_details: await Channel.getAll(),
-            tokenExpiresIn
+            token_expires_in,
         };
-        res.render("steps", page_data);
+        res.render("steps", pageData);
     } catch (error) {
         return res.status(500).send(error.message);
     }
 });
 
-
 // Create Channel for the app and write it to our database for later reference
 router.post("/create_channel", async (req, res) => {
     try {
         debug("Creating a new channel");
-        const {
+        const { id, name, type } = req.body;
+        const response = await util.createChannel(
+            req.user.access_token,
             id,
             name,
             type
-        } = req.body;
-        const response = await util.createChannel(req.user.access_token, id, name, type);
+        );
         if (response.success) {
-            const channel_details = response.data;
-            Channel.add(channel_details);
-            debug("Channel created successfully", channel_details);
+            const channelDetails = response.data;
+            Channel.add(channelDetails);
+            debug("Channel created successfully", channelDetails);
             res.status(200).send(response);
         } else {
             debug("Channel creation failed", "Item creation failed");
             res.status(500).send({
                 success: false,
-                message: "Item creation failed"
+                message: "Item creation failed",
             });
         }
     } catch (e) {
-        debug("Channel creation failed", e)
+        debug("Channel creation failed", e);
         res.status(500).send({
             success: false,
-            message: e.error || e
+            message: e.error || e,
         });
     }
 });
@@ -82,28 +82,28 @@ router.delete("/auth/pipedrive/callback", async (req, res) => {
         await User.remove(req.body.user_id);
         res.status(200).send({
             success: true,
-            message: "User removed"
+            message: "User removed",
         });
     } else {
         res.status(401).send({
             success: false,
-            message: "Unauthorized"
+            message: "Unauthorized",
         });
     }
 });
 
 // Clears the users and channels table
-router.get('/clean_slate', async (req, res) => {
-    debug("Clearning tables and resetting things")
+router.get("/clean_slate", async (req, res) => {
+    debug("Clearning tables and resetting things");
     try {
-        await knex('users').del();
-        await knex('channels').del();
-        res.render('reset', {})
+        await knex("users").del();
+        await knex("channels").del();
+        res.render("reset", {});
     } catch (e) {
         res.status(500).send({
             success: false,
-            message: e
-        })
+            message: e,
+        });
     }
 });
 
